9045dbf984407d3f923590c3185498c7d2557924
MDK <86747696+000MDK@users.noreply.github.com>
2021-11-21 06:23:22 +0100

    Update scripts in head.js

--- Custom JS/scripts in head.js
+++ Custom JS/scripts in head.js
@@ -1,3 +1,4 @@
 var script1 = document.createElement('script');
 script1.type = 'text/javascript';
 script1.src = 'https://domain.tld/script.js';  
+document.getElementsByTagName('head')[0].appendChild(script1);
