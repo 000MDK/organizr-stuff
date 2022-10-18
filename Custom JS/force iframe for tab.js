252ac3e349904abede313f16c5ee8b7cf1a476a7
MDK <86747696+000MDK@users.noreply.github.com>
2022-10-18 23:40:34 +0200

    Update force iframe for tab.js

--- Custom JS/force iframe for tab.js
+++ Custom JS/force iframe for tab.js
@@ -5,6 +5,8 @@ document.getElementsByTagName('head')[0].appendChild(script1);
 var allframes = document.getElementsByTagName('iframe');
 for(var i=0; i<allframes.length; i++){
     allframes[i].setAttribute('is', 'x-frame-bypass');
+    allframes[i].removeAttribute('class');
+    allframes[i].setAttribute('class', 'iframe');
 }
 var alllinks = document.getElementsByTagName('a');
 for(var i=0; i<alllinks.length; i++){
