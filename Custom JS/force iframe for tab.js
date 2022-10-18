8e452b97dc95ea50fe1601a4e84bb7c743b0effc
MDK <86747696+000MDK@users.noreply.github.com>
2022-10-18 23:34:55 +0200

    Update force iframe for tab.js

--- Custom JS/force iframe for tab.js
+++ Custom JS/force iframe for tab.js
@@ -1,15 +1,15 @@
 var script1 = document.createElement('script');
-script1.type = 'text/javascript';
-script1.src = 'https://unpkg.com/x-frame-bypass@1.0.2/x-frame-bypass.js';    
+script1.type = 'module';
+script1.src = 'https://unpkg.com/x-frame-bypass';    
 document.getElementsByTagName('head')[0].appendChild(script1);
-//tab named "Demo", set to iframe - replace "Demo" with your exact tab-name
-var frame_Demo = document.getElementById('frame-Demo');
-if (frame_Demo !== null) { frame_Demo.outerHTML ='<iframe allow=\"clipboard-read; clipboard-write\" allowfullscreen=\"true\" frameborder=\"0\" id=\"frame-Demo\" data-name=\"Demo\" sandbox=\"allow-presentation allow-forms allow-same-origin allow-pointer-lock allow-scripts allow-popups allow-modals allow-top-navigation allow-downloads\" scrolling=\"auto\" src=\"https://daimn.tld/some.html\" is=\"x-frame-bypass\" class=\"iframe\"></iframe>'; }
-/*
-you may want to append
-" onclick=location.reload();
-to your URL in Settings/Tabs, so it's applied when you click the tab (or you  have to reload)
-example: https://something.wontloadinaniframe/ --> https://something.wontloadinaniframe/" onclick=location.reload();
-Be aware, to change that back you need to edit organizr.db!
-This is also way too hacky, there should be a background worker for this (2do).
- */
+var allframes = document.getElementsByTagName('iframe');
+for(var i=0; i<allframes.length; i++){
+    allframes[i].setAttribute('is', 'x-frame-bypass');
+}
+var alllinks = document.getElementsByTagName('a');
+for(var i=0; i<alllinks.length; i++){
+    if (alllinks[i].getAttribute('onclick') !== null)
+    {
+        alllinks[i].setAttribute('onclick', alllinks[i].getAttribute('onclick') + 'location.reload\(\)\;');
+    }
+}
