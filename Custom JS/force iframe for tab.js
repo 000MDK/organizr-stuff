013da5a437dab3e729c9616af203495a08cac9d7
MDK <86747696+000MDK@users.noreply.github.com>
2022-10-19 00:50:52 +0200

    Update force iframe for tab.js

--- Custom JS/force iframe for tab.js
+++ Custom JS/force iframe for tab.js
@@ -1,17 +1,27 @@
 var script1 = document.createElement('script');
 script1.type = 'module';
-script1.src = 'https://unpkg.com/x-frame-bypass';    
+script1.src = 'https://unpkg.com/x-frame-bypass';
+var script2 = document.createElement('script');
 document.getElementsByTagName('head')[0].appendChild(script1);
 var allframes = document.getElementsByTagName('iframe');
 for(var i=0; i<allframes.length; i++){
-    allframes[i].setAttribute('is', 'x-frame-bypass');
+  if (allframes[i].getAttribute('srcdoc') !== null){    
     allframes[i].removeAttribute('class');
     allframes[i].setAttribute('class', 'iframe');
+    allframes[i].setAttribute('is', 'x-frame-bypass');
+    var tmpsrccpy = allframes[i].getAttribute('src');
+    allframes[i].removeAttribute('src');
+    allframes[i].setAttribute('src', tmpsrccpy);
+    if (allframes[i].getAttribute('sandbox') !== null){
+        allframes[i].removeAttribute('sandbox');
+    }
+  }    
 }
 var alllinks = document.getElementsByTagName('a');
 for(var i=0; i<alllinks.length; i++){
-    if (alllinks[i].getAttribute('onclick') !== null)
-    {
+  if (alllinks[i].getAttribute('class') == 'waves-effect' || alllinks[i].getAttribute('class') == 'waves-effect active'){
+    if (alllinks[i].getAttribute('onclick') !== null){
         alllinks[i].setAttribute('onclick', alllinks[i].getAttribute('onclick') + 'location.reload\(\)\;');
     }
+  }
 }
