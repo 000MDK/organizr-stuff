72a3ded099cb83659bcefe320d63ecb050d7eb8d
MDK <86747696+000MDK@users.noreply.github.com>
2022-10-21 02:11:01 +0200

    Update force iframe for tab.js

--- Custom JS/force iframe for tab.js
+++ Custom JS/force iframe for tab.js
@@ -1,27 +1,14 @@
+//enables some sites to be loaded in iframe
+//examples working: Plex app, converse, ycombinator/hacker news
+//examples not working: google news, youtube, myjdownloader
+
 var script1 = document.createElement('script');
 script1.type = 'module';
 script1.src = 'https://unpkg.com/x-frame-bypass';
-var script2 = document.createElement('script');
 document.getElementsByTagName('head')[0].appendChild(script1);
 var allframes = document.getElementsByTagName('iframe');
 for(var i=0; i<allframes.length; i++){
-  if (allframes[i].getAttribute('srcdoc') !== null){    
-    allframes[i].removeAttribute('class');
-    allframes[i].setAttribute('class', 'iframe');
+  if (allframes[i].getAttribute('sandbox') !== null ){    
     allframes[i].setAttribute('is', 'x-frame-bypass');
-    var tmpsrccpy = allframes[i].getAttribute('src');
-    allframes[i].removeAttribute('src');
-    allframes[i].setAttribute('src', tmpsrccpy);
-    if (allframes[i].getAttribute('sandbox') !== null){
-        allframes[i].removeAttribute('sandbox');
-    }
-  }    
-}
-var alllinks = document.getElementsByTagName('a');
-for(var i=0; i<alllinks.length; i++){
-  if (alllinks[i].getAttribute('class') == 'waves-effect' || alllinks[i].getAttribute('class') == 'waves-effect active'){
-    if (alllinks[i].getAttribute('onclick') !== null){
-        alllinks[i].setAttribute('onclick', alllinks[i].getAttribute('onclick') + 'location.reload\(\)\;');
-    }
-  }
+  }  
 }
